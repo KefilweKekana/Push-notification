a2874210c97edd96339ba804343c02665495716f
KefilweKekana <138357648+KefilweKekana@users.noreply.github.com>
2025-12-29 10:10:02 +0200

    Whitelist send_push_notification method for webhooks

--- push_notifications/hooks.py
+++ push_notifications/hooks.py
@@ -6,3 +6,8 @@ app_publisher = "OctaNode"
 app_description = "Firebase Push Notifications for ERPNext"
 app_email = "kefilwe@octanode.co.za"
 app_license = "MIT"
+
+# Whitelist methods for webhooks
+override_whitelisted_methods = {
+    "push_notifications.push_notifications.api.send_push_notification": "push_notifications.push_notifications.api.send_push_notification"
+}
